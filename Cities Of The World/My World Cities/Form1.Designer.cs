22c596ce1dc249d0ca3fcc5a20b125a329135218
TXZCloud <72456766+txzcloud@users.noreply.github.com>
2022-01-06 02:34:17 -0500

    Add files via upload
    This program is an educational application that teaches about the cities of the world. It entails information on the cities of all countries and their corresponding population.

---
+++ Cities Of The World/My World Cities/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace World_Cities
+{
+	partial class Form_worldCities
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label_City = new System.Windows.Forms.Label();
+            this.label_CountryText = new System.Windows.Forms.Label();
+            this.label_PopulationText = new System.Windows.Forms.Label();
+            this.comboBox_City = new System.Windows.Forms.ComboBox();
+            this.label_Population = new System.Windows.Forms.Label();
+            this.comboBox_Country = new System.Windows.Forms.ComboBox();
+            this.countryChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.resButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.countryChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_City
+            // 
+            this.label_City.AutoSize = true;
+            this.label_City.Location = new System.Drawing.Point(230, 20);
+            this.label_City.Name = "label_City";
+            this.label_City.Size = new System.Drawing.Size(35, 17);
+            this.label_City.TabIndex = 1;
+            this.label_City.Text = "City";
+            // 
+            // label_CountryText
+            // 
+            this.label_CountryText.AutoSize = true;
+            this.label_CountryText.Location = new System.Drawing.Point(519, 20);
+            this.label_CountryText.Name = "label_CountryText";
+            this.label_CountryText.Size = new System.Drawing.Size(64, 17);
+            this.label_CountryText.TabIndex = 2;
+            this.label_CountryText.Text = "Country";
+            // 
+            // label_PopulationText
+            // 
+            this.label_PopulationText.AutoSize = true;
+            this.label_PopulationText.Location = new System.Drawing.Point(808, 20);
+            this.label_PopulationText.Name = "label_PopulationText";
+            this.label_PopulationText.Size = new System.Drawing.Size(85, 17);
+            this.label_PopulationText.TabIndex = 3;
+            this.label_PopulationText.Text = "Population";
+            // 
+            // comboBox_City
+            // 
+            this.comboBox_City.FormattingEnabled = true;
+            this.comboBox_City.Location = new System.Drawing.Point(132, 54);
+            this.comboBox_City.Name = "comboBox_City";
+            this.comboBox_City.Size = new System.Drawing.Size(241, 25);
+            this.comboBox_City.TabIndex = 1;
+            this.comboBox_City.SelectionChangeCommitted += new System.EventHandler(this.comboBox_City_SelectionChangeCommitted);
+            // 
+            // label_Population
+            // 
+            this.label_Population.AutoSize = true;
+            this.label_Population.Location = new System.Drawing.Point(808, 62);
+            this.label_Population.Name = "label_Population";
+            this.label_Population.Size = new System.Drawing.Size(142, 17);
+            this.label_Population.TabIndex = 4;
+            this.label_Population.Text = "0                         ";
+            // 
+            // comboBox_Country
+            // 
+            this.comboBox_Country.FormattingEnabled = true;
+            this.comboBox_Country.Location = new System.Drawing.Point(438, 54);
+            this.comboBox_Country.Name = "comboBox_Country";
+            this.comboBox_Country.Size = new System.Drawing.Size(244, 25);
+            this.comboBox_Country.TabIndex = 8;
+            this.comboBox_Country.SelectionChangeCommitted += new System.EventHandler(this.comboBox_Country_SelectionChangeCommitted);
+            // 
+            // countryChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.countryChart.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.countryChart.Legends.Add(legend2);
+            this.countryChart.Location = new System.Drawing.Point(12, 110);
+            this.countryChart.Name = "countryChart";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "City";
+            this.countryChart.Series.Add(series2);
+            this.countryChart.Size = new System.Drawing.Size(1097, 631);
+            this.countryChart.TabIndex = 9;
+            // 
+            // resButton
+            // 
+            this.resButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.resButton.Location = new System.Drawing.Point(12, 54);
+            this.resButton.Name = "resButton";
+            this.resButton.Size = new System.Drawing.Size(93, 52);
+            this.resButton.TabIndex = 10;
+            this.resButton.Text = "Reset";
+            this.resButton.UseVisualStyleBackColor = false;
+            this.resButton.Click += new System.EventHandler(this.resButton_Click);
+            // 
+            // Form_worldCities
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 753);
+            this.Controls.Add(this.resButton);
+            this.Controls.Add(this.countryChart);
+            this.Controls.Add(this.comboBox_Country);
+            this.Controls.Add(this.label_Population);
+            this.Controls.Add(this.comboBox_City);
+            this.Controls.Add(this.label_PopulationText);
+            this.Controls.Add(this.label_CountryText);
+            this.Controls.Add(this.label_City);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form_worldCities";
+            this.Text = "Cities of the World";
+            ((System.ComponentModel.ISupportInitialize)(this.countryChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label label_City;
+		private System.Windows.Forms.Label label_CountryText;
+		private System.Windows.Forms.Label label_PopulationText;
+		private System.Windows.Forms.ComboBox comboBox_City;
+		private System.Windows.Forms.Label label_Population;
+        private System.Windows.Forms.ComboBox comboBox_Country;
+        private System.Windows.Forms.DataVisualization.Charting.Chart countryChart;
+        private System.Windows.Forms.Button resButton;
+    }
+}
+
---
+++ Cities Of The World/My World Cities/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace World_Cities
+{
+	public partial class Form_worldCities : Form
+	{
+		// This holds the raw data read from the City Country file
+		private string[] rawCityCountryData = null;
+		// Here we store the mapping from city to Country
+		Dictionary<string, string> countryOf = null;
+		// Here we store the mapping from city to population
+		Dictionary<string, int> populationOf = null;
+
+		/// <summary>
+		/// Constructor for the main form
+		/// </summary>
+		public Form_worldCities()
+		{
+			// This initializes the form and all of its components
+			InitializeComponent();
+
+			// Now that the form's components have been initialized
+			// We can go load the City Country data
+			loadData();
+			// And then we fill the combo box
+			fillListBox();
+		}
+
+		public class populationCityPair
+		{
+			// The population
+			public int population;
+			// The city
+			public string city;
+			/// <summary>
+			/// Simple constructor for a popuation/City pair
+			/// </summary>
+			/// <param name="pop"></param>
+			/// <param name="city"></param>
+			public populationCityPair(int pop, String city) 
+			{ 
+				// Just set the population and city fields
+				population = pop; 
+				this.city = city; 
+			}
+		};
+
+		//Above code was not manipulated
+		//---------------------------------------------------------------------------------------------------------------------------
+		//Below is my committed code for this project #3
+
+		private void comboBox_City_SelectionChangeCommitted(object sender, EventArgs e)
+		{
+			// Get the selected city;
+			string city = comboBox_City.SelectedItem.ToString();
+			// Set its country by finding the exact string match of the city
+			//reference: https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.combobox.findstringexact?view=net-5.0
+			comboBox_Country.SelectedIndex = comboBox_Country.FindStringExact(countryOf[city]);
+			// Set its population
+			label_Population.Text = populationOf[city].ToString("N0");
+		}
+
+		private void comboBox_Country_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+			//initialized variables
+			// holds the count of each country's pop
+			int countryPop = 0;
+
+			//first before call, to clear the cities from the comboboxes
+			comboBox_City.Items.Clear();
+
+			//get selected country
+			string country = comboBox_Country.SelectedItem.ToString();
+
+			//this is the dict that holds the country's cities
+			Dictionary<string, int> cities = new Dictionary<string, int>();
+
+			//for looping to find each city that exist within a specified country
+			foreach (KeyValuePair<string, string> cityCountry in countryOf)
+			{
+				//checking to see if the citycountry valie matches directly with the country
+				if (cityCountry.Value.Equals(country))
+				{
+					//if true, it will add said city to the combobox for the cities
+					cities.Add(cityCountry.Key, populationOf[cityCountry.Key]);
+				}
+			}
+
+			//--------------------------------------------------------------------------------
+			//piechart customization section below
+
+			//setting the piechart to be visible
+			countryChart.Visible = true;
+			//adding the tooltip so when mouse is hovered, it will show the city's population
+			//reference: https://www.aspforums.net/Threads/119482/Display-data-in-Tooltip-on-Pie-Chart-Mouse-Hover-using-C-and-VBNet-in-ASPNet/
+			countryChart.Series["City"].ToolTip = "Population:  #VALY"; // reference - set tooltip for piechart
+
+			//this is just to disable the labels of piechart due to the unreadable labels overlapping each other on the chart
+			//reference: https://stackoverflow.com/questions/2147930/hide-labels-in-pie-charts-ms-chart-for-net
+			countryChart.Series["City"]["PieLabelStyle"] = "Disabled"; // reference - disable labels of piechart
+
+			//clearing and adding titles for piechart
+			//reference: https://stackoverflow.com/questions/13320181/how-to-set-and-show-title-to-pie-chart-using-ms-chart
+			countryChart.Titles.Clear();
+			countryChart.Titles.Add("PieChart of All Cities Within Selected Country");
+			countryChart.Titles.Add("[Please hover mouse over piechart for tool tip]");
+
+			//this is a separate dict that will sort cities by its population
+			Dictionary<string, int> sortedCities = cities.OrderByDescending(p => p.Value).ToDictionary(p => p.Key, p => p.Value);
+			//clearing the data series to avoid overlapping of data when reselection of country
+			countryChart.Series["City"].Points.Clear();
+
+
+			//for looping to add city pop to data series of chart, also to summate the total population
+			foreach (KeyValuePair<string, int> cityPopulation in sortedCities)
+			{
+				comboBox_City.Items.Add(cityPopulation.Key);
+				countryPop += cityPopulation.Value;
+				countryChart.Series["City"].Points.AddXY(cityPopulation.Key, cityPopulation.Value);
+			}
+			//starting the selected index at 0
+			comboBox_City.SelectedIndex = 0;
+			//displaying the total pop for a country to the label
+			label_Population.Text = countryPop.ToString("N0");
+		}
+
+		//below is the reset button, after each reset, clears the comboboxes and charts
+		private void resButton_Click(object sender, EventArgs e)
+		{
+			//allow the chart's visible to set to false, not showing the chart after reset button is clicked
+			countryChart.Visible = false;
+			//calls the fillListBox function to fill out the cities and countries for the two existing comboboxes
+			fillListBox();
+		}
+
+	}
+}
